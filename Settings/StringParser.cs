f77e8e3477fd9c551ac2177f5605d9e6119e3696
Connor Hilarides <connorcpu@live.com>
2013-05-02 20:44:52 -0400

    Escape literals

--- Settings/StringParser.cs
+++ Settings/StringParser.cs
@@ -1,12 +1,19 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Cloudsdale.Settings {
     public class StringParser {
+        public static readonly Dictionary<char, string> Escapes = new Dictionary<char, string> {
+            {'\r', @"\r"},
+            {'\n', @"\n"},
+            {'\t', @"\t"},
+            {'\\', @"\"}
+        };
+
         public static string ParseLiteral(string input) {
             var result = new StringBuilder(input.Length);
-            char c;
             for (var i = 0; i < input.Length; ++i) {
-                c = input[i];
+                var c = input[i];
                 if (c != '\\') {
                     result.Append(c);
                     continue;
@@ -29,6 +36,21 @@ namespace Cloudsdale.Settings {
                     case '\\':
                         result.Append('\\');
                         break;
+                    default:
+                        result.Append('\\').Append(c);
+                        break;
+                }
+            }
+            return result.ToString();
+        }
+
+        public static string EscapeLiteral(string input) {
+            var result = new StringBuilder(input.Length + 10);
+            foreach (var c in input) {
+                if (Escapes.ContainsKey(c)) {
+                    result.Append(Escapes[c]);
+                } else {
+                    result.Append(c);
                 }
             }
             return result.ToString();
