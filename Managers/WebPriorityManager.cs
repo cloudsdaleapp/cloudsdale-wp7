29d788585b41a510dbaab1a06d330a7f545c5857
Connorcpu <connorcpu@live.com>
2013-05-22 18:32:16 -0400

    Use queues lol

--- Managers/WebPriorityManager.cs
+++ Managers/WebPriorityManager.cs
@@ -15,17 +15,16 @@ using System.Windows.Shapes;
 
 namespace Cloudsdale.Managers {
     public static class WebPriorityManager {
-        private static readonly List<Request> QueueL = new List<Request>();
-        private static readonly List<Request> QueueM = new List<Request>();
-        private static readonly List<Request> QueueH = new List<Request>();
+        private static readonly Queue<Request> QueueL = new Queue<Request>();
+        private static readonly Queue<Request> QueueM = new Queue<Request>();
+        private static readonly Queue<Request> QueueH = new Queue<Request>();
         private static readonly Timer TimerL = new Timer(HandleL, null, 10, 200);
         private static readonly Timer TimerM = new Timer(HandleM, null, 10, 100);
         private static readonly Timer TimerH = new Timer(HandleH, null, 10, 50);
 
         private static void HandleL(object state) {
             if (QueueL.Count == 0) return;
-            var item = QueueL[0];
-            QueueL.RemoveAt(0);
+            var item = QueueL.Dequeue();
             var wc = new WebClient();
             foreach (var header in item.headers) {
                 wc.Headers[header.Key] = header.Value;
@@ -37,8 +36,7 @@ namespace Cloudsdale.Managers {
 
         private static void HandleM(object state) {
             if (QueueM.Count == 0) return;
-            var item = QueueM[0];
-            QueueM.RemoveAt(0);
+            var item = QueueM.Dequeue();
             var wc = new WebClient();
             foreach (var header in item.headers) {
                 wc.Headers[header.Key] = header.Value;
@@ -50,8 +48,7 @@ namespace Cloudsdale.Managers {
 
         private static void HandleH(object state) {
             if (QueueH.Count == 0) return;
-            var item = QueueH[0];
-            QueueH.RemoveAt(0);
+            var item = QueueH.Dequeue();
             var wc = new WebClient();
             foreach (var header in item.headers) {
                 wc.Headers[header.Key] = header.Value;
@@ -63,22 +60,28 @@ namespace Cloudsdale.Managers {
 
         public static void BeginLowPriorityRequest(Uri uri, Action<DownloadStringCompletedEventArgs> callback,
             params KeyValuePair<string, string>[] headers) {
-            QueueL.Add(new Request {
-                uri = uri, callback = callback, headers = headers
+            QueueL.Enqueue(new Request {
+                uri = uri,
+                callback = callback,
+                headers = headers
             });
         }
 
         public static void BeginMediumPriorityRequest(Uri uri, Action<DownloadStringCompletedEventArgs> callback,
             params KeyValuePair<string, string>[] headers) {
-            QueueM.Add(new Request {
-                uri = uri, callback = callback, headers = headers
+            QueueM.Enqueue(new Request {
+                uri = uri,
+                callback = callback,
+                headers = headers
             });
         }
 
         public static void BeginHighPriorityRequest(Uri uri, Action<DownloadStringCompletedEventArgs> callback,
             params KeyValuePair<string, string>[] headers) {
-            QueueH.Add(new Request {
-                uri = uri, callback = callback, headers = headers
+            QueueH.Enqueue(new Request {
+                uri = uri,
+                callback = callback,
+                headers = headers
             });
         }
 
