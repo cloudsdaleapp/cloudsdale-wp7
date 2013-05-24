3d123e4577d3ae3512ea603ff8c316b244caddaf
Connorcpu <connorcpu@live.com>
2013-05-24 11:21:53 -0400

    ack

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: InternalsVisibleTo("Newtonsoft.Json")]
